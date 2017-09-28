d5127aea44f4c8e2bff5d20d8a5c9b803f922f0e
Alex Coulcher <alex@polyglot.rodeo>
2017-09-28 10:53:47 +0100

    Update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,18 +1,4 @@
 import { configure } from '@storybook/react';
-// import { setOptions } from '@storybook/addon-options';
-//
-// setOptions({
-//   name: 'cam0 [front-end]',
-//   goFullScreen: false,
-//   showLeftPanel: true,
-//   showDownPanel: true,
-//   showSearchBox: false,
-//   downPanelInRight: true,
-//   sortStoriesByKind: false,
-//   hierarchySeparator: /\/|\./,
-// });
-//
-
 
 function loadStories() {
   require('../stories');
